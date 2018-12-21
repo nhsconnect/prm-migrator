4eadc0daccff4b3c130552552131b117a19636fb
jdunleav <jdunleav@thoughtworks.com>
2018-12-21 16:08:46 +0000

    PRM-30: Can now successfully accept a message

--- lambda/ehr_extract_handler/main.js
+++ lambda/ehr_extract_handler/main.js
@@ -1,62 +1,111 @@
-const uuid = require('uuid/v4');
-const AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const uuid = require("uuid/v4");
+const AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
-AWS.config.update({region: 'eu-west-2'});
+AWS.config.update({ region: "eu-west-2" });
 
 const MigrationEventStates = {
-    ACCEPTED: "ACCEPTED",
-    REJECTED: "REJECTED",
-    PROCESSING: "PROCESSING",
-    COMPLETED: "COMPLETED",
-    FAILED: "FAILED",
-}
+  ACCEPTED: "ACCEPTED",
+  REJECTED: "REJECTED",
+  PROCESSING: "PROCESSING",
+  COMPLETED: "COMPLETED",
+  FAILED: "FAILED"
+};
 
 class MigrationEventStateMachine {
-    constructor(initialState) {
-        this.state = MigrationEventStates.REJECTED;
-    }
+  constructor(client) {
+    this.uuid = undefined;
+    this.status = MigrationEventStates.REJECTED;
+    this.client = client;
+  }
 
-    get currentState() {
-        return this.state;
+  async accept() {
+    if (this.uuid !== undefined) {
+      throw new Error(
+        `INVALID STATE TRANSITION: Cannot move from ${this.state} to ${
+          MigrationEventStates.ACCEPTED
+        }`
+      );
     }
+    try {
+      const expectedID = uuid();
+      const expectedState = MigrationEventStates.ACCEPTED;
+      const result = await this.client.put({
+        PROCESS_ID: expectedID,
+        PROCESS_STATUS: expectedState
+      });
+      this.uuid = result.PROCESS_ID;
+      this.status = result.PROCESS_STATUS;
+    } catch (err) {
+      console.log(err);
+    }
+    
+    return this;
+  }
+
+  get currentStatus() {
+    return this.status;
+  }
+
+  get correlationId() {
+    return this.uuid;
+  }
 }
 
-exports.main = function() {
-    return new MigrationEventStateMachine()
+class ProcessStatusWrapper {
+  constructor(dbClient) {
+    this.dbClient = dbClient;
+  }
+
+  async put(item) {
+    const result = await this.dbClient
+      .put({
+        TableName: "PROCESS_STORAGE",
+        Item: item,
+        ReturnValues: "ALL_OLD"
+      })
+      .promise();    
+    return item;
+  }
 }
+
+exports.main = async function(dbClient) {
+  const event = new MigrationEventStateMachine(
+    new ProcessStatusWrapper(dbClient)
+  );
+  const result = await event.accept();
+  return event;
+};
 
 const INITIAL_STATUS = "PROCESSING";
-exports.handler = function (event, context, callback) {
-    const uuid = uniqid();
-    const params = {
-        TableName: "PROCESS_STORAGE",
-        Item: {
-            "PROCESS_ID": {S: `${uuid}`},
-            "PROCESS_PAYLOAD": {S: `${INITIAL_STATUS}`}
-        }
-    };
+exports.handler = function(event, context, callback) {
+  const uuid = uniqid();
+  const params = {
+    TableName: "PROCESS_STORAGE",
+    Item: {
+      PROCESS_ID: { S: `${uuid}` },
+      PROCESS_PAYLOAD: { S: `${INITIAL_STATUS}` }
+    }
+  };
 
-    ddb.putItem(params, function (err, data) {
-        if (err) {
-            console.log("Error", err);
-        } else {
-            console.log("Success", data);
-        }
-    });
-    const response = {
-        "statusCode": 200,
-        "headers": {
-            "my_header": "my_value"
-        },
-        "body": JSON.stringify(
-            {
-                "uuid": `${uuid}`,
-                "status": `${INITIAL_STATUS}`
-            }
-        ),
-        "isBase64Encoded": false
-    };
+  ddb.putItem(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
+  const response = {
+    statusCode: 200,
+    headers: {
+      my_header: "my_value"
+    },
+    body: JSON.stringify({
+      uuid: `${uuid}`,
+      status: `${INITIAL_STATUS}`
+    }),
+    isBase64Encoded: false
+  };
 
-    callback(null, response);
+  callback(null, response);
 };
--- lambda/ehr_extract_handler/main.test.js
+++ lambda/ehr_extract_handler/main.test.js
@@ -1,9 +1,41 @@
-const ehrExtract = require('./main');
+const ehrExtract = require("./main");
 
-test("That it integrates with AWS Lambda", () => {});
+class ErrorDBMock {
+  constructor() {}
+
+  put(params, callback) {
+    return {
+      promise: () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {reject()}, 100);
+        });
+      }
+    };
+  }
+}
+
+class DynamoDBMock {
+  constructor() {}
+
+  put(params) {
+    return {
+      promise: () => {
+        return new Promise((resolve, reject) => {
+             setTimeout(() => {resolve()}, 100);
+        });
+      }
+    };
+  }
+}
 
 describe("REJECTED responses", () => {
-  test("That if there is an error when saving the data, it generates a REJECTED response", () => {
-    expect(ehrExtract.main().currentState).toBe("REJECTED");
+  test("That if there is an error when saving the data, it generates a REJECTED response", async () => {
+    const result = await ehrExtract.main(new ErrorDBMock());
+    expect(result.currentStatus).toBe("REJECTED");
+  });
+
+  test("That if there is no error, it generates an ACCEPTED response", async () => {
+    const result = await ehrExtract.main(new DynamoDBMock());
+    expect(result.currentStatus).toBe("ACCEPTED");
   });
 });
