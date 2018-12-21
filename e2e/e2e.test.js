7d15a805019b4340ecfaf451a1fe70b10e4ed0ce
jdunleav <jdunleav@thoughtworks.com>
2018-12-21 17:01:20 +0000

    PRM-30: Updated e2e tests

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -15,34 +15,18 @@ test("That PRM tells us when we use the wrong endpoint", async () => {
 });
 
 test("That PRM tells us that it is, broadly speaking, working", async () => {
-  const response = await request.post(`${PRM_URL.origin}/dev`, {
+  const response = await request.post(`${PRM_URL.origin}/sendMessage`, {
     resolveWithFullResponse: true
   });
   expect(response.statusCode).toBe(200);
 });
 
-test("That we get an ack back from the request with a unique identifier", async () => {
-  const response = await request.post(`${PRM_URL.origin}/dev`, {
+test("That we get an ack back from the request with a unique identifier and status", async () => {
+  const response = await request.post(`${PRM_URL.origin}/sendMessage`, {
     resolveWithFullResponse: true
   });
   expect(response.statusCode).toBe(200);
-  const { uuid } = JSON.parse(response.body)
+  const { uuid, status } = JSON.parse(response.body)
   expect(uuid).toBeDefined();
-
-
-  // const response2 = await request.get(`${PRM_URL.origin}/status?uuid=${uuid}`, {
-  //   resolveWithFullResponse: true
-  // });
-  // expect(response2.statusCode).toBe(200);
-  // const { status2 } = JSON.parse(response2.body)
-  // expect(status2).toBe("PROCESSING");
-  //
-  // // wait 1 sec
-  //
-  // const response3 = await request.get(`${PRM_URL.origin}/status?uuid=${uuid}`, {
-  //   resolveWithFullResponse: true
-  // });
-  // expect(response3.statusCode).toBe(200);
-  // const { status3 } = JSON.parse(response3.body)
-  // expect(status3).toBe("COMPLETED");
+  expect(status).toBe("ACCEPTED");
 });
