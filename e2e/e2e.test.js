baa623d76c5d7070c4508c5133b14aa079fc53f3
jdunleav <jdunleav@thoughtworks.com>
2018-12-21 17:21:25 +0000

    PRM-30: Refactoring e2e tests

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -4,25 +4,10 @@ const errors = require("request-promise-native/errors");
 
 const PRM_URL = new Url.URL(process.env.PRM_ENDPOINT);
 
-test("That PRM tells us when we use the wrong endpoint", async () => {
-  expect.assertions(2);
-  try {
-    await request.post(`${PRM_URL.origin}/bleh`);
-  } catch (e) {
-    expect(e.statusCode).toBe(403);
-    expect(e instanceof errors.StatusCodeError).toBeTruthy();
-  }
-});
-
-test("That PRM tells us that it is, broadly speaking, working", async () => {
-  const response = await request.post(`${PRM_URL.origin}/sendMessage`, {
-    resolveWithFullResponse: true
-  });
-  expect(response.statusCode).toBe(200);
-});
-
-test("That we get an ack back from the request with a unique identifier and status", async () => {
-  const response = await request.post(`${PRM_URL.origin}/sendMessage`, {
+test("As a supplier, I can see my message has been accepted", async () => {
+  const url = `${PRM_URL.origin}${PRM_URL.pathname}/send`;
+  console.log(url);
+  const response = await request.post(url, {
     resolveWithFullResponse: true
   });
   expect(response.statusCode).toBe(200);
