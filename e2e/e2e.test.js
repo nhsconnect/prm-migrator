5b3db770f6bc93b978166cc3031b07dd16b0b391
jdunleav <jdunleav@thoughtworks.com>
2018-12-21 17:42:46 +0000

    PRM-30: Added e2e test for showing supplier message is being processed

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,6 +1,7 @@
 const Url = require("url");
 const request = require("request-promise-native");
 const errors = require("request-promise-native/errors");
+const sleep = m => new Promise(r => setTimeout(r, m))
 
 const PRM_URL = new Url.URL(process.env.PRM_ENDPOINT);
 
@@ -14,4 +15,20 @@ test("As a supplier, I can see my message has been accepted", async () => {
   const { uuid, status } = JSON.parse(response.body)
   expect(uuid).toBeDefined();
   expect(status).toBe("ACCEPTED");
+});
+
+test("As a supplier, I can see my message is being processed", async () => {
+  const sendUrl = `${PRM_URL.origin}${PRM_URL.pathname}/send`;
+  const sendResponse = await request.post(sendUrl, {
+    resolveWithFullResponse: true
+  });
+  const { uuid } = JSON.parse(sendResponse.body)
+  await sleep(1000);
+  
+  const statusUrl = `${PRM_URL.origin}${PRM_URL.pathname}/status/${uuid}`;
+  const statusResponse = await request.get(statusUrl, {
+    resolveWithFullResponse: true
+  });
+  const { status } = JSON.parse(statusResponse.body)  
+  expect(status).toBe("PROCESSING");
 });
