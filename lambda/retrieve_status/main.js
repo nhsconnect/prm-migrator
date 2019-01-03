c52dacbf33dd54ef60b120afdb3eb0d060330eb6
cynthiatw <leec@thoughtworks.com>
2019-01-03 16:09:45 +0000

    PRM-30: Change retrievestatus/main.js to update the status of a process with given id to PROCESSING in main.handler

--- lambda/retrieve_status/main.js
+++ lambda/retrieve_status/main.js
@@ -5,6 +5,8 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
     const uuid = event.pathParameters.uuid;
 
+    updateStatusToProcessing(uuid, callback);
+
     const params = {
         TableName: "PROCESS_STORAGE",
         Key: {
@@ -17,28 +19,11 @@ exports.handler = function (event, context, callback) {
 
     ddb.get(params, function (err, data) {
 
-
         if (err) {
-            var response = {
-                "statusCode": 500,
-                "headers": {
-                    "my_header": "my_value"
-                },
-                "body": JSON.stringify(
-                    {
-                        "uuid": uuid,
-                        "status": `${status}`
-                    }),
-                "isBase64Encoded": false
-            };
-            console.log("Error", err);
-            callback(null, response);
-
+            handleError(err, uuid, callback)
         } else {
             status = data.Item.PROCESS_STATUS;
-            console.log(status)
             console.log("Success", data);
-
 
             var response = {
                 "statusCode": 200,
@@ -58,3 +43,42 @@ exports.handler = function (event, context, callback) {
     });
 
 };
+
+function updateStatusToProcessing(uuid, callback) {
+    const params = {
+        TableName: "PROCESS_STORAGE",
+        Key: {
+            "PROCESS_ID":  uuid
+        },
+        UpdateExpression: "set PROCESS_STATUS = :p",
+        ExpressionAttributeValues:{
+            ":p":"PROCESSING",
+        },
+        ReturnValues:"UPDATED_NEW"
+    };
+    ddb.update(params, function(err, data) {
+        if (err) {
+            handleError(err, uuid, callback)
+        } else {
+            let status = data.Attributes;
+            console.log("Update Data Attributes", data);
+            return;
+        }
+    });
+}
+
+function handleError(err, uuid, callback) {
+    var response = {
+        "statusCode": 500,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": JSON.stringify(
+            {
+                "message": `Could not retrieve the process status for process id ${uuid}.`
+            }),
+        "isBase64Encoded": false
+    };
+    console.log("Error", err);
+    callback(null, response);
+}
