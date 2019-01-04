ba95a6635f00dc08edc534439af5e1f4df83325b
cynthiatw <leec@thoughtworks.com>
2019-01-04 12:31:07 +0000

    PRM-30: Adjust e2e tests to call the /send endpoint with a payload in the body of the request

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,26 +1,48 @@
 const Url = require("url");
 const request = require("request-promise-native");
 const errors = require("request-promise-native/errors");
-const sleep = m => new Promise(r => setTimeout(r, m))
+const sleep = m => new Promise(r => setTimeout(r, m));
 
 const PRM_URL = new Url.URL(process.env.PRM_ENDPOINT);
 
 test("As a supplier, I can see my message has been accepted", async () => {
     const url = `${PRM_URL.origin}${PRM_URL.pathname}/send`;
-    const response = await request.post(url, {
+    const testPayload = "foo";
+
+    var options = {
+        method: 'POST',
+        uri: url,
+        body: JSON.stringify({
+            payload: testPayload
+        }),
         resolveWithFullResponse: true
-    });
+    };
+
+    const response = await request.post(options);
+
     expect(response.statusCode).toBe(200);
-    const {uuid, status} = JSON.parse(response.body);
+    const {uuid, status, payload} = JSON.parse(response.body);
     expect(uuid).toBeDefined();
     expect(status).toBe("ACCEPTED");
+    expect(payload).toBe('foo');
 });
 
 test("As a supplier, I can see my message is being processed", async () => {
     const sendUrl = `${PRM_URL.origin}${PRM_URL.pathname}/send`;
-    const sendResponse = await request.post(sendUrl, {
+
+    const testPayload = "foo";
+
+    var options = {
+        method: 'POST',
+        uri: sendUrl,
+        body: JSON.stringify({
+            payload: testPayload
+        }),
         resolveWithFullResponse: true
-    });
+    };
+
+    const sendResponse = await request.post(options);
+
     const {uuid} = JSON.parse(sendResponse.body);
     await sleep(1000);
 
