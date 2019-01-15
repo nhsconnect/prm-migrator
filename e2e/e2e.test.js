edcc60651670a00487c8cbcd6379039899c768b1
Christian Taylor <ctaylor@thoughtworks.com>
2019-01-15 10:15:24 +0000

    PRM-30: add e2e test for retrieve payload

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -6,10 +6,10 @@ const sleep = m => new Promise(r => setTimeout(r, m));
 const PRM_URL = new Url.URL(process.env.PRM_ENDPOINT);
 
 let testUuid;
+const testPayload = "foo";
 
-test("As a supplier, I can see my message has been accepted", async () => {
+test("As a supplier, I can send my message and see that it has been accepted", async () => {
     const url = `${PRM_URL.origin}${PRM_URL.pathname}/send`;
-    const testPayload = "foo";
 
     var options = {
         method: 'POST',
@@ -30,42 +30,58 @@ test("As a supplier, I can see my message has been accepted", async () => {
     expect(payload).toBe('foo');
 });
 
-test("As a supplier, I can see my message is being processed", async () => {
-    const sendUrl = `${PRM_URL.origin}${PRM_URL.pathname}/status/${testUuid}`;
-
-    console.log(sendUrl);
+test("As a supplier, I can see my message has been accepted", async () => {
+    const statusUrl = `${PRM_URL.origin}${PRM_URL.pathname}/status/${testUuid}`;
 
     var options = {
         method: 'GET',
-        uri: sendUrl,
+        uri: statusUrl,
         resolveWithFullResponse: true
     };
 
-    await request.get(options);
+    const response = await request.get(options);
+
+    const {status} = JSON.parse(response.body);
+    expect(status).toBe("ACCEPTED");
+});
+
+test("As a supplier, I can see my message is being processed", async () => {
+    const statusUrl = `${PRM_URL.origin}${PRM_URL.pathname}/status/${testUuid}`;
+
+    var options = {
+        method: 'GET',
+        uri: statusUrl,
+        resolveWithFullResponse: true
+    };
+
     const response = await request.get(options);
 
     const {status} = JSON.parse(response.body);
     expect(status).toBe("PROCESSING");
 });
 
-test.skip("As a supplier, I can retrieve my processed ehrExtract in form of a payload", async () => {
-    const sendUrl = `${PRM_URL.origin}${PRM_URL.pathname}/send`;
-    const sendResponse = await request.post(sendUrl, {
-        resolveWithFullResponse: true
-    });
-    const {uuid} = JSON.parse(sendResponse.body);
-    await sleep(1000);
 
-    const statusUrl = `${PRM_URL.origin}${PRM_URL.pathname}/status/${uuid}`;
-    const statusResponse = await request.post(statusUrl, {
-        resolveWithFullResponse: true
-    });
+test("As a supplier, I can see my message has been completed", async () => {
+    const statusUrl = `${PRM_URL.origin}${PRM_URL.pathname}/status/${testUuid}`;
 
-    const retrieveUrl = `${PRM_URL.origin}${PRM_URL.pathname}/retrieve/${uuid}`;
+    var options = {
+        method: 'GET',
+        uri: statusUrl,
+        resolveWithFullResponse: true
+    };
+
+    const response = await request.get(options);
+
+    const {status} = JSON.parse(response.body);
+    expect(status).toBe("COMPLETED");
+});
+
+test("As a supplier, I can retrieve my processed ehrExtract in form of a payload", async () => {
+    const retrieveUrl = `${PRM_URL.origin}${PRM_URL.pathname}/retrieve/${testUuid}`;
     const retrieveResponse = await request.post(retrieveUrl, {
         resolveWithFullResponse: true
     });
-
-    const {processedEhrExtract} = JSON.parse(retrieveResponse.body);
-    expect(processedEhrExtract).toBeDefined();
+    
+    console.log(retrieveResponse.body);
+    expect(retrieveResponse.body).toBe(`{"payload":"${testPayload}"}`);
 });
