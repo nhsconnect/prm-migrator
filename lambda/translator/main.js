96069bdb0f4fb26acbb1ccd9c0f7287bd8a4bf96
cynthiatw <leec@thoughtworks.com>
2019-01-18 16:35:50 +0000

    PRM-21: refactor code into translate module

--- lambda/translator/main.js
+++ lambda/translator/main.js
@@ -1,31 +1,8 @@
-const AWS = require('aws-sdk');
-AWS.config.update({ region: "eu-west-2" });
-const dbQueryHelper = require('./dbQueryHelper');
-const EasyXml = require('easyxml');
+const translate = require('./translate');
 
 exports.handler = (event, context) => {
-    let nhsNumber = event.dynamodb.NewImage.PROCESS_PAYLOAD.S;
-    let uuid = event.dynamodb.Keys.PROCESS_ID.S;
 
-    let client = new AWS.DynamoDB.DocumentClient();
-
-    client.update(dbQueryHelper.changeStatusTo('PROCESSING', uuid));
-
-    let payload = {
-        identifier: {
-            value: nhsNumber
-        }
-    };
-
-    var patientPayloadXmlOptions = {
-        rootElement: 'Patient',
-        manifest: true,
-    };
-    let serializer = new EasyXml(patientPayloadXmlOptions);
-    let payloadXml = serializer.render(payload);
-
-    client.update(dbQueryHelper.changePayloadTo(payloadXml, uuid));
-    client.update(dbQueryHelper.changeStatusTo('COMPLETED', uuid));
+    translate.blat(event);
 
     return {
         statusCode: 200,
--- lambda/translator/translate.js
+++ lambda/translator/translate.js
@@ -1,3 +1,29 @@
-exports.handler = function(){
+const AWS = require('aws-sdk');
+AWS.config.update({ region: "eu-west-2" });
+const dbQueryHelper = require('./dbQueryHelper');
+const EasyXml = require('easyxml');
 
+exports.blat = function(event){
+    let client = new AWS.DynamoDB.DocumentClient();
+
+    let nhsNumber = event.dynamodb.NewImage.PROCESS_PAYLOAD.S;
+    let uuid = event.dynamodb.Keys.PROCESS_ID.S;
+
+    client.update(dbQueryHelper.changeStatusTo('PROCESSING', uuid));
+
+    let payload = {
+        identifier: {
+            value: nhsNumber
+        }
+    };
+
+    var patientPayloadXmlOptions = {
+        rootElement: 'Patient',
+        manifest: true,
+    };
+    let serializer = new EasyXml(patientPayloadXmlOptions);
+    let payloadXml = serializer.render(payload);
+
+    client.update(dbQueryHelper.changePayloadTo(payloadXml, uuid));
+    client.update(dbQueryHelper.changeStatusTo('COMPLETED', uuid));
 };
