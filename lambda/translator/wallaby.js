d3a5f2b415650dda36ba681360ec9df20917d95d
Christian Taylor <ctaylor@thoughtworks.com>
2019-01-18 20:32:47 +0000

    PRM-21: fix wallaby config file

--- lambda/translator/wallaby.js
+++ lambda/translator/wallaby.js
@@ -1,11 +1,16 @@
 /* ignore file coverage: just a config file */
 module.exports = () => {
     return {
+      loose: true,
       files: [
         '*.js', 
-        '!*.test.js'
+        'test/*.js', 
+        '!test/*.test.js'
       ],
-      tests: ['*.test.js'],
+      tests: [
+        // '!test/*.js',
+        'test/*.test.js'
+        ],
       hints: {
         ignoreCoverageForFile: /ignore file coverage/,
         ignoreCoverage: /ignore coverage/
