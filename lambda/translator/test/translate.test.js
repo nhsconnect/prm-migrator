a60ead8fbbee772c04ccb83dc44dbe69dc024f6e
Christian Taylor <ctaylor@thoughtworks.com>
2019-01-21 10:33:28 +0000

    PRM-21: move translate tests to its own suite

--- lambda/translator/test/translate.test.js
+++ lambda/translator/test/translate.test.js
@@ -1,13 +1,48 @@
 const translator = require("../translate");
+const given = require("./given")
+const AWS = require('aws-sdk-mock');
+const sinon = require('sinon');
+const dbQueryHelper = require('../dbQueryHelper');
+const EasyXml = require('easyxml');
 
-describe("When translating a payload", () => {
-  let result;  
+describe("Calling lambda", () => {
+  let result;
+  var updateSpy = sinon.spy();
 
   beforeAll(() => {
-    let event = {};
+    AWS.mock('DynamoDB.DocumentClient', 'update', updateSpy);
+    result = translator.blat(given.aNewRecord);
   });
-  
-  test("something", async () => {
-    expect(result).toBe(undefined);
+
+  afterAll(() => {
+    AWS.restore('DynamoDB.DocumentClient');
+  });
+
+  test("it should update the status to PROCESSING", async () => {
+    var expectedParams = dbQueryHelper.changeStatusTo('PROCESSING', '101');
+    expect(updateSpy.calledWith(expectedParams)).toBeTruthy();
+  });
+
+  test("it should update the payload with transformed xml", async () => {
+    var patientPayloadXmlOptions = {
+      rootElement: 'Patient',
+      manifest: true,
+    };
+    
+    let xmlSerializer = new EasyXml(patientPayloadXmlOptions);
+
+    let payload = {
+        identifier: {
+            value: '1234567890'
+        }
+    };
+
+    var expectedParams = dbQueryHelper.changePayloadTo(xmlSerializer.render(payload), '101');
+    expect(updateSpy.calledWith(expectedParams)).toBeTruthy();
+  });
+
+  test("it should update status to COMPLETED", async () => {
+    var expectedParams = dbQueryHelper.changeStatusTo('COMPLETED', '101');
+    expect(updateSpy.calledWith(expectedParams)).toBeTruthy();
   });
 });
