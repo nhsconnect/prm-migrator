5b3f53a3361509b9a39c16de5ee882eef51c4127
Christian Taylor <ctaylor@thoughtworks.com>
2019-02-14 16:13:18 +0000

    PRM-101: add happy and unhappy tests for pds_client

--- lambda/translator/pds_client.js
+++ lambda/translator/pds_client.js
@@ -1,5 +1,14 @@
+const soapRequest = require('easy-soap-request');
+
 exports.verifyNhsNumber = async function (nhsNumber) {
-    return new Promise((resolve, reject) => {
-        resolve(true);
-    });
+    const url = 'https://graphical.weather.gov/xml/SOAP_server/ndfdXMLserver.php';
+    const headers = {
+        'user-agent': 'sampleTest',
+        'Content-Type': 'text/xml;charset=UTF-8',
+        'soapAction': 'https://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl#LatLonListZipCode',
+    };
+
+    let xml = 'put the real xml in here please';
+    const { response } = await soapRequest(url, headers, xml, 10000); //?
+    return response.body.includes('SMSP-0000');
 }
--- lambda/translator/test/pds_client.test.js
+++ lambda/translator/test/pds_client.test.js
@@ -1,9 +1,20 @@
 const pds_client = require('../pds_client');
+const given = require('./given');
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
 
 describe("PDS calls work", () => {
-
     test('we should be able to get an adequate response when validating a valid NHS number', async () => {
         const nhsNoToVerify = "9999345201";
+        let mock = new MockAdapter(axios);
+        mock.onPost().reply(200, given.generateValidPdsResponseFor(nhsNoToVerify));
         expect(await pds_client.verifyNhsNumber(nhsNoToVerify)).toBe(true);
+    });
+
+    test('we should be able to get an adequate response when validating a invalid NHS number', async () => {
+        const nhsNoToVerify = "9999345201";
+        let mock = new MockAdapter(axios);
+        mock.onPost().reply(200, given.generateInvalidPdsResponseFor(nhsNoToVerify));
+        expect(await pds_client.verifyNhsNumber(nhsNoToVerify)).toBe(false);
     });
 });
