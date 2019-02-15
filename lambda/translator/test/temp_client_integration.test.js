3517d4790df41685fa7788994839ddeda883e6a8
Christian Taylor <ctaylor@thoughtworks.com>
2019-02-15 16:14:08 +0000

    PRM-101: skip integration tests

--- lambda/translator/test/temp_client_integration.test.js
+++ lambda/translator/test/temp_client_integration.test.js
@@ -1,6 +1,6 @@
 const pds_client = require('../temp_client');
 
-describe("PDS calls actually do work", () => {
+describe.skip("PDS calls actually do work", () => {
     test('we should be able to get an adequate response when validating a valid NHS number', async () => {
         const nhsNoToVerify = "9999345201";
         expect(await pds_client.verifyNhsNumber(nhsNoToVerify)).toBe(true);
