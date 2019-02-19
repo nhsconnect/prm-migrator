0306089f1be5de6afe7252a63e153d2f4c539593
Christian Taylor <ctaylor@thoughtworks.com>
2019-02-19 17:28:13 +0000

    PRM-101: rename variable to not be the same as function name

--- lambda/translator/temp_client.js
+++ lambda/translator/temp_client.js
@@ -14,9 +14,9 @@ exports.verifyNhsNumber = async function (nhsNumber) {
     let xmlRequest = this.generateRequest(nhsNumber);
     let timeout_ms = 3000;
     const { response } = await soapRequest(`${url}/${path}`, headers, xmlRequest, timeout_ms);
-    let isValid = isValid(response.body);
-    console.log(`Response from PDS validity is: ${isValid}`);
-    return isValid;
+    let responseValidity = isValid(response.body);
+    console.log(`Response from PDS validity is: ${responseValidity}`);
+    return responseValidity;
 }
 
 function isValid(xmlResponse) {
