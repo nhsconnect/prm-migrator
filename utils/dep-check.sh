18db1a7ab44cea20b396baacc0bada4b5713936b
Christian Taylor <ctaylor@thoughtworks.com>
2019-02-27 16:38:43 +0000

    PRMT-81: exit with 1 when vulnerabilities are found

--- utils/dep-check.sh
+++ utils/dep-check.sh
@@ -3,7 +3,7 @@
 npm-recursive-install
 
 echo "Starting scan"
-/usr/share/dependency-check/bin/dependency-check.sh --project prm-migrator --scan . --out /report --format ALL
+/usr/share/dependency-check/bin/dependency-check.sh --project prm-migrator --scan . --out /report --format ALL --failOnCVSS 0
 echo "Scan complete"
 
 export VULS=$(cat /report/dependency-check-report.json | jq '.dependencies | .[] | select(.vulnerabilities != null) | {fileName, filePath, vulnerabilities:.vulnerabilities[] | {cvssScore:.cvssScore | tonumber,severity}}' | jq -n '[inputs]')
@@ -13,5 +13,7 @@ export HIGH_VULS=$(echo $VULS | jq '.[] | .vulnerabilities | select(.cvssScore >
 
 if [ "$HIGH_VULS" ]; then
   echo "Vulnerabilities found!"
-  export CODEBUILD_BUILD_SUCCEEDING=0
+  exit 1
 fi
+
+exit 0
