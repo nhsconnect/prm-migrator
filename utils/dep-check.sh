f5fcf6f583b76232a49111cebbf33554589bcc3d
Christian Taylor <ctaylor@thoughtworks.com>
2019-02-27 17:01:42 +0000

    PRMT-81: pull in FAIL_ON_CVSS env var

--- utils/dep-check.sh
+++ utils/dep-check.sh
@@ -3,13 +3,13 @@
 npm-recursive-install
 
 echo "Starting scan"
-/usr/share/dependency-check/bin/dependency-check.sh --project prm-migrator --scan . --out /report --format ALL --failOnCVSS 0
+/usr/share/dependency-check/bin/dependency-check.sh --project prm-migrator --scan . --out /report --format ALL --failOnCVSS $FAIL_ON_CVSS
 echo "Scan complete"
 
 export VULS=$(cat /report/dependency-check-report.json | jq '.dependencies | .[] | select(.vulnerabilities != null) | {fileName, filePath, vulnerabilities:.vulnerabilities[] | {cvssScore:.cvssScore | tonumber,severity}}' | jq -n '[inputs]')
 echo $VULS
 
-export HIGH_VULS=$(echo $VULS | jq '.[] | .vulnerabilities | select(.cvssScore >= 0.0)')
+export HIGH_VULS=$(echo $VULS | jq '.[] | .vulnerabilities | select(.cvssScore >= $FAIL_ON_CVSS)')
 
 if [ "$HIGH_VULS" ]; then
   echo "Vulnerabilities found!"
