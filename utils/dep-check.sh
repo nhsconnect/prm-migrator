f4a66c8f9f12d87feeedbee5e92451aea88c5c97
Christian Taylor <ctaylor@thoughtworks.com>
2019-02-27 16:20:45 +0000

    PRMT-81: fix type in script

--- utils/dep-check.sh
+++ utils/dep-check.sh
@@ -2,14 +2,16 @@
 
 npm-recursive-install
 
+echo "Starting scan"
 /usr/share/dependency-check/bin/dependency-check.sh --project prm-migrator --scan . --out /report --format ALL
+echo "Scan complete"
 
 export VULS=$(cat /report/dependency-check-report.json | jq '.dependencies | .[] | select(.vulnerabilities != null) | {fileName, filePath, vulnerabilities:.vulnerabilities[] | {cvssScore:.cvssScore | tonumber,severity}}' | jq -n '[inputs]')
-
-echo $VUL
+echo $VULS
 
 export HIGH_VULS=$(echo $VULS | jq '.[] | .vulnerabilities | select(.cvssScore >= 0.0)')
 
 if [ "$HIGH_VULS" ]; then
+  echo "Vulnerabilities found!"
   export CODEBUILD_BUILD_SUCCEEDING=0
 fi
